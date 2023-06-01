80af7493e6493c8e4fbd9530a5ef7e636d300cb3
Roshan <roshangupta2020@gmail.com>
2023-06-01 15:34:35 +0530

    Day 24 and 25 updated

---
+++ Day25-1091. Shortest Path in Binary Matrix.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//problem link 
+//https://leetcode.com/problems/shortest-path-in-binary-matrix/
+
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+
+        int n = grid.size();
+        vector<vector<int>> visited(n, vector<int>(n, 0));
+        if(grid[0][0]!=0 || grid[n-1][n-1]!=0){
+            return -1;
+        }
+        queue<pair<int,int>> q;
+        q.push({0,0});
+        int res = 0;
+        while(!q.empty()){
+            int m = q.size();
+            for(int i=0; i<m; i++){
+                pair<int,int>p = q.front();
+                int r = p.first;
+                int c = p.second;
+                q.pop();
+                if(r<0 || r>=n || c<0 || c>=n || grid[r][c]==1 || visited[r][c]){
+                    continue;
+                }
+                if(r==n-1 && c==n-1){
+                    return res+1;
+                }
+                visited[r][c] = 1;
+                q.push({r-1,c-1});
+                q.push({r-1,c});
+                q.push({r-1,c+1});
+                q.push({r,c-1});
+                q.push({r,c+1});
+                q.push({r+1,c-1});
+                q.push({r+1,c});
+                q.push({r+1,c+1});
+            }
+            res++;   
+        }
+        return -1;
+    }
+};
