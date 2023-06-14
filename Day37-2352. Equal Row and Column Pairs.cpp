65315af1972ed9fb406eb6e63a67acc0049d06df
Roshan <roshangupta2020@gmail.com>
2023-06-14 13:07:30 +0530

    Day 37 solution 2 updates using map

--- Day37-2352. Equal Row and Column Pairs.cpp
+++ Day37-2352. Equal Row and Column Pairs.cpp
@@ -31,6 +31,26 @@ public:
         
     }
 };
+//Solution 2 using map<vector<int> , int>
+
+class Solution2 {
+public:
+    int equalPairs(vector<vector<int>>& grid) {
+        map<vector<int> , int>mp;
+        int ans = 0;
+        int n = grid.size();
+        for(int i=0;i<n;i++)  mp[grid[i]]++;
+        
+        for(int j=0;j<n;j++){
+            vector<int>col;
+            for(int i=0;i<n;i++){
+                col.push_back(grid[i][j]);
+                if(mp.find(col) != mp.end()) ans+=mp[col];
+            }
+        }
+        return ans;
+    }
+};
 
 /*
 2352. Equal Row and Column Pairs
