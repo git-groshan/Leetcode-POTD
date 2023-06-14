c359a32d7b20c7df99135a2dc5f55f3583c96d8d
Roshan <roshangupta2020@gmail.com>
2023-06-14 15:05:41 +0530

    day 38 modified

--- Day38-530. Minimum Absolute Difference in BST.cpp
+++ Day38-530. Minimum Absolute Difference in BST.cpp
@@ -4,17 +4,17 @@ using namespace std;
 //Problem link 
 //https://leetcode.com/problems/minimum-absolute-difference-in-bst/
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
 class Solution {
 public:
     void inorder(TreeNode* root , int &prev  , int &ans ){
